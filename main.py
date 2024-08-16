84a7f0548917245f68d106394e02d78d0fb45e52
Talank <talank@gces.edu.np>
2024-08-15 22:03:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ def main():
         print("Usage: python main.py <yaml-file>")
         sys.exit(1)
     yaml_file_path = sys.argv[1]
+    output_file_path = sys.argv[2]
+    
     yaml_file_content = utils.get_file_content(yaml_file_path)
-
     modified_file = utils.modify_file_content(yaml_file_content)
+
+    with open(output_file_path, 'w') as f:
+        f.write(modified_file)
+    
     print(modified_file)
 
 
